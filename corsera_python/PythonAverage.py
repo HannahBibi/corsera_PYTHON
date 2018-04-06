427628e0a6c2e7841aae8292ece85ad0c9dd25e0
Hannah <hannah@Aeryn.connect>
2018-04-06 11:09:44 +0100

    Yeet

---
+++ corsera_python/PythonAverage.py
@@ -0,0 +1,13 @@
+from corsera_python.PythonSum import sum
+from corsera_python.PythonCount import count
+
+def avg (l):
+    curr_sum = 0
+    curr_count = 0
+    for i in l:
+        curr_count = curr_count + 1
+        curr_sum = curr_sum + i
+    return curr_sum / curr_count
+
+def avg2 (l):
+    return sum(l) / count(l)
