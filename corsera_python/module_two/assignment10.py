57ea9f1d9f304600eb9f3cd399763d491b1d29e4
Hannah <hannah@Aeryn.connect>
2018-04-10 16:51:09 +0100

    yeet

---
+++ corsera_python/module_two/assignment10.py
@@ -0,0 +1,13 @@
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+count = 0
+running_total = 0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    pos = line.find("0")
+    n = line[pos: ]
+    count = count + 1
+    running_total = running_total+float(n)
+print ("Average spam confidence", running_total / count)
