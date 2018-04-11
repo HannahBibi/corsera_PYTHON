e447c9d734fdf29c409df3a36b483814b8517dbb
Hannah <hannah@Aeryn.connect>
2018-04-11 12:06:57 +0100

    Added assignments 10 through 12

---
+++ corsera_python/module_two/assignment12.py
@@ -0,0 +1,9 @@
+count = 0
+fname = input("Enter file name:")
+file_handle = open(fname)
+for line in file_handle:
+    if 'From ' in line:
+        words = line.split()
+        count = count + 1
+        print(words[1])
+print('There were', count, 'lines in the file with From as the first word')
