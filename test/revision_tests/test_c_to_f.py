4986bcedddebbc8ba24e68716407be7cec769754
Hannah <hannah@Aeryn.connect>
2018-04-27 09:38:44 +0100

    Refactored test names, as well as disposed of redundant tests and added new ones.

--- test/revision_tests/test_c_to_f.py
+++ test/revision_tests/test_c_to_f.py
@@ -10,6 +10,12 @@ class ConvertTemp(unittest.TestCase):
     def test_minus_fourty(self):
         self.assertEqual(-40, calculate_farenheit(-40))
 
+    def test_positive_number(self):
+        self.assertEqual(138.2, calculate_farenheit(59))
+
+    def test_three_decimal_places(self):
+        self.assertEqual(157.0226, calculate_farenheit(69.457))
+
 
 if __name__ == '  main  ':
     unittest.main()
--- test/revision_tests/test_f_to_c.py
+++ test/revision_tests/test_f_to_c.py
@@ -7,14 +7,15 @@ class ConvertTemp(unittest.TestCase):
     def test_zero_temp(self):
         self.assertEqual(-17.77777777777778, calculate_celsius(0))
 
-    def test_minus_fourty(self):
+    def test_for_a_negative_number(self):
         self.assertEqual(-40, calculate_celsius(-40))
 
-    def test_one_hundred(self):
+    def test_for_a_positive_number(self):
         self.assertEqual(37.77777777777778, calculate_celsius(100))
 
-    def test_seventy_two(self):
-        self.assertEqual(22.22222222222222, calculate_celsius(72))
+    def test_for_three_decimal_places(self):
+        self.assertEqual(-15.917777777777777, calculate_celsius(3.348))
+
 
 
 if __name__ == '  main  ':
