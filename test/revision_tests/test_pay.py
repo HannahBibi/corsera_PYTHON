cda4de6658203189b0c28105a06cb4b840bebeef
Hannah <hannah@Aeryn.connect>
2018-04-30 16:09:14 +0100

    Added more to the tests to make them more easily parsable.

--- test/revision_tests/test_pay.py
+++ test/revision_tests/test_pay.py
@@ -5,73 +5,73 @@ from coursera_python.revision.revision_pay_rate import normal_pay, overtime_pay,
 class CalculateNormalPay(unittest.TestCase):
 
     def test_with_no_overtime(self):
-        self.assertEqual(400, normal_pay(40, 10))
+        self.assertEqual(400, normal_pay(hours_worked=40, rate_of_pay=10))
 
     def test_with_minus_hours(self):
-        self.assertEqual(0, normal_pay(-40, 10))
+        self.assertEqual(0, normal_pay(hours_worked= -40, rate_of_pay=10))
 
     def test_with_minus_rate(self):
-        self.assertEqual(0, normal_pay(40, -10))
+        self.assertEqual(0, normal_pay(hours_worked=40, rate_of_pay= -10))
 
     def test_with_zero_hours(self):
-        self.assertEqual(0, normal_pay(0, 10))
+        self.assertEqual(0, normal_pay(hours_worked=0, rate_of_pay=10))
 
     def test_with_zero_rate(self):
-        self.assertEqual(0, normal_pay(10, 0))
+        self.assertEqual(0, normal_pay(hours_worked=10, rate_of_pay=0))
 
     def test_with_zero(self):
-        self.assertEqual(0, normal_pay(0, 0))
+        self.assertEqual(0, normal_pay(hours_worked=0, rate_of_pay=0))
 
     def test_with_less_than_fourty(self):
-        self.assertEqual(350, normal_pay(35, 10))
+        self.assertEqual(350, normal_pay(hours_worked=35, rate_of_pay=10))
 
     def test_greater_than_fourty(self):
-        self.assertEqual(400, normal_pay(45, 10))
+        self.assertEqual(400, normal_pay(hours_worked=45, rate_of_pay=10))
 
     def test_diff_rate_greater_than_fourty(self):
-        self.assertEqual(376.0, normal_pay(89, 9.4))
+        self.assertEqual(376.0, normal_pay(hours_worked=89, rate_of_pay=9.4))
 
 
 class CalculateOvertimePay(unittest.TestCase):
 
     def test_with_fourty_five(self):
-        self.assertEqual(75.0, overtime_pay(45, 10))
+        self.assertEqual(75.0, overtime_pay(hours_worked=45, rate_of_pay=10))
 
     def test_with_zero(self):
-        self.assertEqual(0, overtime_pay(0, 0))
+        self.assertEqual(0, overtime_pay(hours_worked=0, rate_of_pay=0))
 
     def test_with_minus_rate(self):
-        self.assertEqual(0, overtime_pay(45, -8))
+        self.assertEqual(0, overtime_pay(hours_worked=45, rate_of_pay= -8))
 
     def test_with_zero_hours(self):
-        self.assertEqual(0, overtime_pay(0, 10))
+        self.assertEqual(0, overtime_pay(hours_worked=0, rate_of_pay=10))
 
     def test_with_less_than_fourty_five(self):
-        self.assertEqual(0, overtime_pay(24, 10))
+        self.assertEqual(0, overtime_pay(hours_worked=24, rate_of_pay=10))
 
     def test_with_minus_hours(self):
-        self.assertEqual(0, overtime_pay(-12, 10))
+        self.assertEqual(0, overtime_pay(hours_worked= -12, rate_of_pay=10))
 
 
 class TotalPay(unittest.TestCase):
 
     def test_with_fourty_five(self):
-        self.assertEqual(475.0, total_pay(45, 10))
+        self.assertEqual(475.0, total_pay(hours_worked=45, rate_of_pay=10))
 
     def test_with_zero(self):
-        self.assertEqual(0, total_pay(0, 0))
+        self.assertEqual(0, total_pay(hours_worked=0, rate_of_pay=0))
 
     def test_with_minus_hours(self):
-        self.assertEqual(0, total_pay(-45, 10))
+        self.assertEqual(0, total_pay(hours_worked= -45, rate_of_pay=10))
 
     def test_with_minus_rate(self):
-        self.assertEqual(0, total_pay(45, -10))
+        self.assertEqual(0, total_pay(hours_worked=45, rate_of_pay= -10))
 
     def test_with_less_than_fourty_five(self):
-        self.assertEqual(240, total_pay(24, 10))
+        self.assertEqual(240, total_pay(hours_worked=24, rate_of_pay=10))
 
     def test_with_more_than_fourty_five(self):
-        self.assertEqual(3400.0, total_pay(240, 10))
+        self.assertEqual(3400.0, total_pay(hours_worked=240, rate_of_pay=10))
 
 
 if __name__ == '  main  ':
