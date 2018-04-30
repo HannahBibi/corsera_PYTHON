f7f1de72f8c0d11d06cb678223be2b8c121b3689
Hannah <hannah@Aeryn.connect>
2018-04-30 16:13:25 +0100

    Added smaller than or equal to zero in the rate of pay code.

--- coursera_python/revision/revision_pay_rate.py
+++ coursera_python/revision/revision_pay_rate.py
@@ -1,5 +1,5 @@
 def normal_pay(hours_worked, rate_of_pay):
-    if hours_worked < 0 or rate_of_pay < 0:
+    if hours_worked <= 0 or rate_of_pay <= 0:
         return 0
     elif hours_worked > 40:
         return 40 * rate_of_pay
@@ -8,7 +8,7 @@ def normal_pay(hours_worked, rate_of_pay):
 
 
 def overtime_pay(hours_worked, rate_of_pay):
-    if hours_worked < 0 or rate_of_pay < 0:
+    if hours_worked <= 0 or rate_of_pay <= 0:
         return 0
     elif hours_worked < 40:
         return 0
