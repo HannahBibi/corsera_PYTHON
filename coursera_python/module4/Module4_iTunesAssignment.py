fabc6124ace5db1b4b386684c998f9dfb8bd4ec8
Hannah <hannah@Aeryn.connect>
2018-05-24 18:05:33 +0100

    Adjusted as per Issue iTunesAssignment.py up to spec

--- coursera_python/module4/Module4_iTunesAssignment.py
+++ coursera_python/module4/Module4_iTunesAssignment.py
@@ -1,16 +1,21 @@
 import xml.etree.ElementTree as ET
 import sqlite3
 
-conn = sqlite3.connect('trackdb.sqlite')
-cur = conn.cursor()
+db_connect = sqlite3.connect('trackdb.sqlite')
+cursor = db_connect.cursor()
 
-# Make some fresh tables using executescript()
-cur.executescript('''
+cursor.executescript('''
 DROP TABLE IF EXISTS Artist;
 DROP TABLE IF EXISTS Album;
 DROP TABLE IF EXISTS Track;
+DROP TABLE IF EXISTS Genre;
 
 CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Genre (
     id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name    TEXT UNIQUE
 );
@@ -26,57 +31,65 @@ CREATE TABLE Track (
         AUTOINCREMENT UNIQUE,
     title TEXT  UNIQUE,
     album_id  INTEGER,
+    genre_id  INTEGER,
     len INTEGER, rating INTEGER, count INTEGER
 );
 ''')
 
 
-fname = input('Enter file name: ')
-if ( len(fname) < 1 ) : fname = 'Library.xml'
+file_name = 'Library.xml'
 
-# <key>Track ID</key><integer>369</integer>
-# <key>Name</key><string>Another One Bites The Dust</string>
-# <key>Artist</key><string>Queen</string>
+
 def lookup(d, key):
     found = False
     for child in d:
-        if found : return child.text
-        if child.tag == 'key' and child.text == key :
+        if found:
+            return child.text
+        if child.tag == 'key' and child.text == key:
             found = True
     return None
 
-stuff = ET.parse(fname)
-all = stuff.findall('dict/dict/dict')
-print('Dict count:', len(all))
-for entry in all:
-    if ( lookup(entry, 'Track ID') is None ) : continue
+
+library_xml = ET.parse(file_name)
+entries = library_xml.findall('dict/dict/dict')
+for entry in entries:
+    if lookup(entry, 'Track ID') is None:
+        continue
 
     name = lookup(entry, 'Name')
     artist = lookup(entry, 'Artist')
     album = lookup(entry, 'Album')
-    count = lookup(entry, 'Play Count')
+    play_count = lookup(entry, 'Play Count')
     rating = lookup(entry, 'Rating')
     length = lookup(entry, 'Total Time')
+    genre = lookup(entry, 'Genre')
 
-    if name is None or artist is None or album is None :
-        continue
+    if name is None \
+        or artist is None \
+        or album is None \
+        or genre is None:
+            continue
 
-    print(name, artist, album, count, rating, length)
+    print(name, artist, album, play_count, rating, length, genre)
 
-    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
-        VALUES ( ? )''', ( artist, ) )
-    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
-    artist_id = cur.fetchone()[0]
+    cursor.execute('''INSERT OR IGNORE INTO Artist (name) 
+        VALUES ( ? )''', (artist, ))
+    cursor.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
+    artist_id = cursor.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
-        VALUES ( ?, ? )''', ( album, artist_id ) )
-    cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
-    album_id = cur.fetchone()[0]
+    cursor.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+        VALUES ( ?, ? )''', ( album, artist_id))
+    cursor.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
+    album_id = cursor.fetchone()[0]
 
-    cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count) 
-        VALUES ( ?, ?, ?, ?, ? )''',
-        ( name, album_id, length, rating, count ) )
+    cursor.execute('''INSERT OR IGNORE INTO Genre (name) 
+        VALUES ( ? )''', (genre, ))
+    cursor.execute('SELECT id FROM Genre WHERE name = ?', (genre, ))
+    genre_id = cursor.fetchone()[0]
 
-    conn.commit()
+    cursor.execute('''INSERT OR REPLACE INTO Track
+        (title, album_id, len, rating, count, genre_id) 
+        VALUES ( ?, ?, ?, ?, ?, ? )''',
+        (name, album_id, length, rating, play_count, genre_id))
 
+    db_connect.commit()
